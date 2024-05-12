20fe5bbe4dc46f764f17cc16f4bba1d49a0790fb
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-05-12 14:21:51 +0530

    Initial setup of the app and added custom component to show details.

---
+++ src/app/new-meetup/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
